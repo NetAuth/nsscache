608f807c663f25304d5681895fde0e4672b882d9
Michael Aldridge <aldridge.mac@gmail.com>
2018-07-15 00:41:28 -0700

    Bail out if the maps can't be refreshed

--- cmd/nsscache/main.go
+++ cmd/nsscache/main.go
@@ -229,10 +229,12 @@ func main() {
 	entList, err := nacl.ListGroupMembers("ALL")
 	if err != nil {
 		log.Println(err)
+		os.Exit(1)
 	}
 	grpList, err := nacl.ListGroups("", false)
 	if err != nil {
 		log.Println(err)
+		os.Exit(2)
 	}
 
 	// Turn the group list into a map so that we can fetch these
